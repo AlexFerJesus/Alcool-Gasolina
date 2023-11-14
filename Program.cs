8acd7c5a42ae7e3f20bff9881dade45acf926c89
AlexFerJesus <alexfernandesdejesus.12@gmail.com>
2023-11-14 10:16:43 -0300

    Esqueci de salvar ;-;

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("-----Alcool ou Gasolina?-----");
+
+Console.WriteLine("Digite o preço do alcool (R$):");
+decimal Alcool = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite o preço da gasolina (R$):");
+decimal Gasolina = Convert.ToDecimal(Console.ReadLine());
+
+string Recomendacao;
+
+if (AbastecerComGasolina (Alcool, Gasolina))
+{
+    Recomendacao = "Gasolina";
+}
+else
+{
+    Recomendacao = "Alcool";
+}
+
+double razao = RazaoAlcoolGasolina(Alcool, Gasolina) * 100;
+
+Console.WriteLine ($"O Preço do alcool corresponde a {razao:N1}% do preço da gasolina");
+Console.WriteLine($"\nRecomendação: Abasteça com {Recomendacao.ToUpper()}.");
+
+double RazaoAlcoolGasolina(decimal precoAlcool, decimal precoGasolina)
+{
+    return Convert.ToDouble(precoAlcool / precoGasolina);
+}
+
+bool AbastecerComGasolina(decimal precoAlcool, decimal precoGasolina)
+{
+    const double MinRazUsoGas = 0.73;
+    return RazaoAlcoolGasolina(precoAlcool, precoGasolina) >= MinRazUsoGas;
+}
